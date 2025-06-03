6dd10a6b135cee9ddb2dd79fd375ff33d8709002
jozzydgr8 <jozzydgreat1@gmail.com>
2025-06-03 02:10:42 -0700

    fixed booking issue and paystack metadata to test next

--- src/Pages/BookingPage.tsx
+++ src/Pages/BookingPage.tsx
@@ -256,8 +256,8 @@ function generateRecurringDates(
 
 };
 
-  // const publicKey = process.env.REACT_APP_Pay_PublicKey!;
-  const publicKey ='pk_test_0e745897d2bb51a12c4fca668a094dcecd425aea';
+  const publicKey = process.env.REACT_APP_Pay_PublicKey!;
+  // const publicKey ='pk_test_0e745897d2bb51a12c4fca668a094dcecd425aea';
 
   const componentProp = {
   email: dataSubmit.email,
